2a91e5940bfded293564603e4e2541870347028b
Gughyy <g.serpenti1997@gmail.com>
2023-06-09 23:14:23 +0200

    EnemyVero

---
+++ Zombie Lost in time/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float MaxHealth;
+    [SerializeField] private float currentHealth;
+
+
+    void Start()
+    {
+        currentHealth = MaxHealth;
+    }
+
+
+    public void TakeDamage(float Damage)
+    {
+        currentHealth -= Damage;
+        if (currentHealth <= 0)
+        {
+            Debug.Log("Enemy dead");
+            //currentHealth = MaxHealth;
+            Destroy(gameObject);
+        }
+    }
+}
