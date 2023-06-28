c5abbc5e8863df313838c5a882633c7193226c0a
CosimoDL <cosimodeluna17@gmail.com>
2023-06-28 18:44:39 +0200

    fix

--- Zombie Lost in time/Assets/Scripts/Cosimo/ButtonHandlerForExit.cs
+++ Zombie Lost in time/Assets/Scripts/Cosimo/ButtonHandlerForExit.cs
@@ -1,10 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
-using UnityEngine.UIElements;
 
 public class ButtonHandlerForExit : MonoBehaviour
 {
