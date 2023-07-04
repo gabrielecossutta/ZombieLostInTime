6fdda3372ccd9223b3db2766cc200f9f0e526eaa
gabrielecossutta <cossuttagabriele@gmail.com>
2023-07-05 01:54:51 +0200

    fix bullet

--- Zombie Lost in time/Assets/Scripts/Gabriele/Proiettili/Proiettile rotante/BulletRotatingPowerUp.cs
+++ Zombie Lost in time/Assets/Scripts/Gabriele/Proiettili/Proiettile rotante/BulletRotatingPowerUp.cs
@@ -7,11 +7,11 @@ public class BulletRotatingPowerUp : Singleton<BulletRotatingPowerUp>
     public float Range = 2f;
     public float Speed = 3f;
     public float Damage = 5f;
-    public float rotationSpeed = -1f;
+    public float rotationSpeed;
     void Update()
     {
         transform.position = GameObject.FindGameObjectWithTag("Player").transform.position + new Vector3(Mathf.Sin(Time.time*Speed)*Range, 1.5f, Mathf.Cos(Time.time*Speed)*Range);
-        transform.Rotate(Vector3.right * rotationSpeed);
+        transform.Rotate(Vector3.forward * rotationSpeed);
     }
     private void OnTriggerEnter(Collider other)
     {
